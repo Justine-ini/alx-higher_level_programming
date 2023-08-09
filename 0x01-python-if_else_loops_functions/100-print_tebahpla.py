864e86edb41b395c7e2a71f476d879bc71d82300
Justine-Ini <inijustine4040@gmail.com>
2023-08-09 08:06:14 -0700

    Bugfix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(26):
     if i % 2 == 0:
-        print("{:c}".format(ord('z') - i), end='') 
+        print("{:c}".format(ord('z') - i), end='')
     else:
         print("{:c}".format(ord('Z') - i), end='')
