71604718dc16340c51e07f1ef43c08abd89c6649
Justine-Ini <inijustine4040@gmail.com>
2023-08-11 12:27:02 -0700

    Bugfix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
-    num_args = len(sys.argv)
-    if num_args != 4:
+    from calculator_1 import add, sub, mul, div
+
+    argv = sys.argv[1:]
+    if len(argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
-    if op == '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op == '-':
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op == '*':
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op == '/':
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
-    else:
+        sys.exit(1)
+    a, op, b = argv
+    if op not in ('+', '-', '*', '/'):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
+    a = int(a)
+    b = int(b)
+    if op == '+':
+        res = add(a, b)
+    elif op == '-':
+        res = sub(a, b)
+    elif op == '*':
+        res = mul(a, b)
+    else:
+        res = div(a, b)
+    print("{} {} {} = {}".format(a, op, b, res))
 
