213fc74698bf725694fdf9e15bccd4b9f0f300a4
Justine-ini <inijustine4040@gmail.com>
2023-09-06 04:15:46 -0700

    Bugfix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,9 +12,9 @@ def add_integer(a, b=98):
         The sum of a and b.
     """
 
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, int) or not isinstance(a, float):
         raise TypeError('a must be an integer or float')
-    if not isinstance(b, int) and not isinstance(a, float):
+    if not isinstance(b, int) or not isinstance(a, float):
         raise TypeError('b must be an integer or float')
 
     a = int(a)
