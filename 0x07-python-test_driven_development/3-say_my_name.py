ba0ad5eea76cd77d7990b29c47780ae6361159f3
Justine-ini <inijustine4040@gmail.com>
2023-09-08 10:43:49 -0700

    Bugfix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,6 +15,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError('first_name must be a string')
 
     if not isinstance(last_name, str):
-        last_name = ""
+        raise TypeError("last_name must be a string")
 
     print(f'My name is {first_name} {last_name}')
