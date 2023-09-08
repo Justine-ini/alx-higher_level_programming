d0d74d53595e1fa9f551d5abbff0a710500fd784
Justine-ini <inijustine4040@gmail.com>
2023-09-08 11:07:16 -0700

    Bugfix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+
+This module is composed by a function that divides the numbers of a matrix
+
+"""
+
+
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix.
@@ -9,11 +16,18 @@ def matrix_divided(matrix, div):
 
     Returns:
         A new matrix with all elements divided by div.
-    """
 
+    Raises:
+        TypeError: If the elements of the matrix aren't lists
+                   If the elemetns of the lists aren't integers/floats
+                   If div is not an integer/float number
+                   If the lists of the matrix don't have the same size
+
+        ZeroDivisionError: If div is zero
+
+    """
     if not isinstance(matrix, list):
         raise TypeError('matrix must be a list of lists')
-
     for row in matrix:
         if not isinstance(row, list):
             raise TypeError('matrix must be a list of lists')
