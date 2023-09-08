915e729e22c61e92ca9d6b6b25b639aba55c007e
Justine-ini <inijustine4040@gmail.com>
2023-09-08 10:50:01 -0700

    Bugfix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
-    """
-    Prints the user's name.
+    """ Function that prints "My name is <first name> <last name>"
 
     Args:
-        first_name (str): The user's first name.
-        last_name (str, optional): The user's last name.
-        Defaults to an empty string.
+        first_name: first name
+        last_name: last name
+
+    Returns:
+        No return
 
     Raises:
-        TypeError: If first_name or last_name is not a string.
+        TypeError: If first_name or last_name is not a string
+
+
     """
+
 
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
