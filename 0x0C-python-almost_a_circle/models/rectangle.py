44c8b2f63e6549ff376c54458095070d23f1395d
Justine-ini <inijustine4040@gmail.com>
2023-09-19 09:51:39 -0700

    Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -120,14 +120,14 @@ class Rectangle(Base):
         num_args = len(args)
         if num_args = 0:
             raise ValueError("At least one argument is required.")
-
-        if num_args >= 1:
-            self.id = args[0]
-        if num_args >= 2:
-            self.width = args[1]
-        if num_args >= 3:
-            self.height = args[2]
-        if num_args >= 4:
-            self.x = args[3]
-        if num_args >= 5:
-            self.y = args[4]
+        for arg in num_args:
+            if arg == 1:
+                self.id = args[0]
+            if arg == 2:
+                self.width = args[1]
+            if arg == 3:
+                self.height = args[2]
+            if arg == 4:
+                self.x = args[3]
+            if arg == 5:
+                self.y = args[4]
