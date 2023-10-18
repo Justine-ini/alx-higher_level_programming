a5bdb581957bbda0c3a5fa5d87e81ef6c1b2ec8f
Justine-ini <inijustine4040@gmail.com>
2023-10-17 18:17:50 -0700

    Bugfix

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Creates a table in the current database in your MySQL server.
 
-CREATE TABLES IF NOT EXIST `first_table`(`id` INT, `name` VARCHAR(256))
+CREATE TABLE IF NOT EXIST `first_table`(`id` INT, `name` VARCHAR(256));
